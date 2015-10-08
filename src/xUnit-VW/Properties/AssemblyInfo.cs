68caf35bd0f8ad671acc3c665a926ed3d3f2d95f
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-10-08 09:17:07 +0200

    Added gitattribute, gitignore and basic structure
    Includes .gitattribute, .gitignore, skeleton project

---
+++ src/xUnit-VW/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("xUnit-VW")]
+[assembly: AssemblyDescription("xUnit.net extension for Volkswagen-like testing.")]
+[assembly: AssemblyProduct("xUnit-VW")]
+[assembly: AssemblyCopyright("Copyright © Eirik M Hammerstad 2015")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("f355d099-d4b2-4acf-b8fd-3c7b2177f8b9")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
